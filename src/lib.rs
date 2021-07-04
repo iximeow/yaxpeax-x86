2c84821ee0d9807d20ea82b8550edde09426a867
iximeow <me@iximeow.net>
2021-07-04 12:55:21 -0700

    add real-mode decoder

--- src/lib.rs
+++ src/lib.rs
@@ -80,7 +80,7 @@ pub use long_mode::Arch as x86_64;
 pub mod protected_mode;
 pub use protected_mode::Arch as x86_32;
 
-mod real_mode;
+pub mod real_mode;
 pub use real_mode::Arch as x86_16;
 
 const MEM_SIZE_STRINGS: [&'static str; 64] = [
