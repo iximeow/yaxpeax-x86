f039d09e4bb118033199b6703e0b6f2333e80bb1
iximeow <me@iximeow.net>
2026-06-10 05:09:47 +0000

    pextr*/pinsr*/insertps/extrps immediate is now u8 instead of i8

--- src/long_mode/mod.rs
+++ src/long_mode/mod.rs
@@ -6741,7 +6741,7 @@ fn read_operands<
             } else {
                 instruction.regs[1].bank = RegisterBank::X;
             }
-            instruction.operands[2] = OperandSpec::ImmI8;
+            instruction.operands[2] = OperandSpec::ImmU8;
             instruction.operand_count = 3;
         }
         OperandCase::G_Ev_xmm_Ib => {
@@ -6760,7 +6760,7 @@ fn read_operands<
             } else {
                 instruction.regs[1].bank = RegisterBank::X;
             }
-            instruction.operands[2] = OperandSpec::ImmI8;
+            instruction.operands[2] = OperandSpec::ImmU8;
             instruction.operand_count = 3;
         }
         OperandCase::PMOVX_E_G_xmm => {
